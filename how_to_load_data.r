6f1104da4d5d8031a8d805b4a27f3c10f959c442
Sara Lakhdar <slakhdar1@hotmail.com>
2012-11-30 14:55:27 -0500

    t.test in MASS library

---
+++ how_to_load_data.r
@@ -0,0 +1,13 @@
+library(MASS)
+snails
+data()
+ t.test(snails$Exposure,snails$Temp)
+data()
+library(MASS)
+ cement
+t.test(cement$x1,cement$x4)
+ library(MASS)
+data()
+ Rabbit
+t.test(Rabbit$BPchange,Rabbit$dose)
+
